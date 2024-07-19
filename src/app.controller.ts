0abfaf2677656e5cfa4a016f15de587e2d613051
GEFR00 <elizabehtriquelme@gmail.com>
2024-07-19 00:33:32 -0400

    [ADD] controller para obtener y crear artista

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,37 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Artistas } from './models/artistas';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  artistas: Artistas[] = [];
+
+  constructor(private readonly appService: AppService) {
+
+    let artista1 = new Artistas(1, "The Beatles", "Sin bio" , "banda", "Reino Unido", "Rock", 1000000);
+    let artista2 = new Artistas(2, "Queen", "Sin bio", "banda", "Reino Unido", "Rock", 900000);
+
+    this.artistas = [
+      artista1, 
+      artista2
+    ];
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('artistas')
+  obtenerArtistas(): Artistas[] {
+    return this.artistas;
+  }
+
+  @Post('artistas')
+  crearArtista( @Body() artista: Artistas ) {
+    
+    artista.id = this.artistas.length + 1;
+    this.artistas.push(artista);
   }
 }
---
+++ src/models/artistas.ts
@@ -0,0 +1,28 @@
+export class Artistas {
+    public id: number;
+    public nombre: string;
+    public biografia: string;
+    public tipo: "artista" | "banda" | "grupo";
+    public pais: string;
+    public generoMusical: string;
+    public oyentesMensuales: number;
+
+    constructor(
+        id: number, 
+        nombre: string, 
+        biografia: string,
+        tipo: "artista" | "banda" | "grupo", 
+        pais: string, 
+        generoMusical: string, 
+        oyentesMensuales: number
+    ) {
+        this.id = id;
+        this.nombre = nombre;
+        this.biografia = biografia;
+        this.tipo = tipo;
+        this.pais = pais;
+        this.generoMusical = generoMusical;
+        this.oyentesMensuales = oyentesMensuales;
+    }
+
+}
